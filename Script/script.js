93586388748708fab9935d204366479bdad80543
joaquim-22 <joaquimfsilva22@gmail.com>
2022-07-27 01:02:01 +0200

    add text and page recrut

--- Script/script.js
+++ Script/script.js
@@ -4,6 +4,10 @@ const joaquim = document.getElementById("joaquim")
 const ayoube = document.getElementById("ayoube")
 const thomas = document.getElementById("thomas")
 const jeff = document.getElementById("jeff")
+const infoJoaquim = document.getElementById("infoJoaquim")
+const infoAyoube = document.getElementById("infoAyoube")
+const infoJeff = document.getElementById("infoJeff")
+const infoThomas = document.getElementById("infoThomas")
 
 
 
@@ -11,7 +15,14 @@ const jeff = document.getElementById("jeff")
 
 // Create functions//
 
-
+function toggleText(name){
+    if (name.style.display === "none") {
+        name.style.display = "block";
+    }
+    else {
+        name.style.display = "none";
+    }
+  }
 
 
 
@@ -21,22 +32,27 @@ const jeff = document.getElementById("jeff")
 
 // Make listener//
 
-joaquim.addEventListener("click", () => {
+joaquim.addEventListener("click", (e) => {
+    toggleText(infoJoaquim)
     ayoube.classList.toggle("test")
     thomas.classList.toggle("test")
     jeff.classList.toggle("test")
+
 })
-ayoube.addEventListener("click", () => {
+ayoube.addEventListener("click", (e) => {
+    toggleText(infoAyoube)
     joaquim.classList.toggle("test")
     thomas.classList.toggle("test")
     jeff.classList.toggle("test")
 })
-thomas.addEventListener("click", () => {
+thomas.addEventListener("click", (e) => {
+    toggleText(infoThomas)
     ayoube.classList.toggle("test")
     joaquim.classList.toggle("test")
     jeff.classList.toggle("test")
 })
-jeff.addEventListener("click", () => {
+jeff.addEventListener("click", (e) => {
+    toggleText(infoJeff)
     ayoube.classList.toggle("test")
     thomas.classList.toggle("test")
     joaquim.classList.toggle("test")
